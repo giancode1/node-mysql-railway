b75bf67cdc807831bffb97c838c57aa07cb6ca48
Giancarlo Culcay <gieg5c@gmail.com>
2022-10-18 12:29:05 -0500

    agregado variables a usar

---
+++ README.md
@@ -0,0 +1,1 @@
+## Ejemplo super basico para usar Railway
--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import { createPool } from 'mysql2/promise'
+import { DB_HOST, DB_PASSWORD, DB_NAME, DB_PORT, DB_USER } from './config.js'
 
 export const pool = createPool({
-    user: 'root',
-    password: '12345',
-    host: 'localhost',
-    port: 3306,
-    database: 'usersdb'
+    user: DB_USER ,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
 })
 
 
